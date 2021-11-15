b2f43316bc682da6b3c0e7cff8cd834c2142597b
Shintarou Okumura <shintarou.okumula@gmail.com>
2021-11-15 19:52:17 +0900

    update overlay

--- overlays.nix
+++ overlays.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ vim-fish }:
 
-{
+let 
+  vim= import ./vim.nix { inherit vim-fish; };
+in {
   nixpkgs.overlays = [
-    # vim.nix overlay, how did i pass the flake package
-
+    vim
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/neovim-nightly-overlay/archive/1dd99a6c91b4a6909e66d0ee69b3f31995f38851.tar.gz;
       sha256 = "1z8gx1cqd18s8zgqksjbyinwgcbndg2r6wv59c4qs24rbgcsvny9";

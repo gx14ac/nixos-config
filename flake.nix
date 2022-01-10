f1c16eb4fad6a26a88acc4266cc51bd9aff49a6d
shinta <flekystyley@gmail.com>
2022-01-10 14:27:19 +0900

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,17 @@
   description = "shintard NixOS system enviroment and tools";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.05";
+      url = "github:nix-community/home-manager/release-21.11";
+
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
     theme-bobthefish = {
       url = "github:oh-my-fish/theme-bobthefish";
